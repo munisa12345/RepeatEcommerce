15108ea5037e1fe4cbc93a5769673c73802bc50e
user <munisaabdurahimova3@gmail.com>
2024-11-30 09:51:44 +0500

    Order object and Status enum classes

--- src/main/java/uz/pdp/repeatecommerce/entity/Product.java
+++ src/main/java/uz/pdp/repeatecommerce/entity/Product.java
@@ -1,4 +1,25 @@
 package uz.pdp.repeatecommerce.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    private Integer id = idGen++;
+    private String name;
+    private Double price;
+    private String photoUrl;
+    private Integer categoryId;
+
+    private static Integer idGen = 1;
+
+    public Product(String name, Double price, String photoUrl, Integer categoryId) {
+        this.name = name;
+        this.price = price;
+        this.photoUrl = photoUrl;
+        this.categoryId = categoryId;
+    }
 }
