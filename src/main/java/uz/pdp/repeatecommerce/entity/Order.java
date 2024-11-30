e3332434b54072432bc0df124de2aa864d094c06
user <munisaabdurahimova3@gmail.com>
2024-11-30 09:35:54 +0500

    Order object and Status enum classes

--- src/main/java/uz/pdp/repeatecommerce/entity/Order.java
+++ src/main/java/uz/pdp/repeatecommerce/entity/Order.java
@@ -1,4 +1,25 @@
 package uz.pdp.repeatecommerce.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+    private Integer id = idGen++;
+    private Status status;
+    private LocalDateTime date;
+    private Integer userId;
+
+    private static Integer idGen = 1;
+
+    public Order(Status status, LocalDateTime date, Integer userId) {
+        this.status = status;
+        this.date = date;
+        this.userId = userId;
+    }
 }
---
+++ src/main/java/uz/pdp/repeatecommerce/entity/Status.java
@@ -0,0 +1,7 @@
+package uz.pdp.repeatecommerce.entity;
+
+public enum Status {
+    NEW,
+    COMPLETED,
+    ARCHIVED
+}
