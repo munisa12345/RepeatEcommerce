ca6ceaf0979d0f55e22c21c1e8c0acd527903d75
user <munisaabdurahimova3@gmail.com>
2024-11-30 09:35:54 +0500

    Category object

--- src/main/java/uz/pdp/repeatecommerce/entity/Category.java
+++ src/main/java/uz/pdp/repeatecommerce/entity/Category.java
@@ -1,4 +1,18 @@
 package uz.pdp.repeatecommerce.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+    private Integer id = idGen++;
+    private String name;
+    private static Integer idGen = 1;
+
+    public Category(String name) {
+        this.name = name;
+    }
 }
